b4e47276f6a95f19be25e745e6f5cf01e71c4d8e
LarsBoertjes <larsboertjes@outlook.com>
2024-02-19 21:06:44 +0100

    Finished step 3 and copied step 4
    copied step 4 for the labelling of the faces from Ken, check it to see how and if it works

--- hw1.cpp
+++ hw1.cpp
@@ -8,6 +8,7 @@
 #include <CGAL/Triangulation_vertex_base_2.h>
 #include <CGAL/Triangulation_face_base_with_info_2.h>
 #include <CGAL/linear_least_squares_fitting_3.h>
+#include <CGAL/Plane_3.h>
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
 typedef CGAL::Exact_predicates_tag Tag;
@@ -23,6 +24,9 @@ typedef CGAL::Constrained_triangulation_face_base_2<Kernel> FaceBase;
 typedef CGAL::Triangulation_face_base_with_info_2<FaceInfo, Kernel, FaceBase> FaceBaseWithInfo;
 typedef CGAL::Triangulation_data_structure_2<VertexBase, FaceBaseWithInfo> TriangulationDataStructure;
 typedef CGAL::Constrained_Delaunay_triangulation_2<Kernel, TriangulationDataStructure, Tag> Triangulation;
+typedef Kernel::Point_2 Point2;
+typedef Kernel::Point_3 Point3;
+
 
 //const std::string input_file = "/Users/ken/Downloads/hw1/NL.IMBAG.Pand.0503100000032914-0.obj"; // the faculty building
 const std::string input_file = "/mnt/c/Users/LarsB/OneDrive/Documenten/GitHub/GEO1004_ass_1/hw1_obj_files/NL.IMBAG.Pand.0503100000000138-0.obj"; // simple test file
@@ -42,7 +46,6 @@ struct Face {
 };
 
 int main(int argc, const char * argv[]) {
-    std::cout << "test" << std::endl;
     std::vector<Vertex> vertices;
     std::vector<Face> faces;
 
@@ -54,7 +57,7 @@ int main(int argc, const char * argv[]) {
 
         // Parse line by line
         while (getline(input_stream, line)) {
-            std::cout << line << std::endl;
+            //std::cout << line << std::endl;
 
             std::istringstream line_stream(line);
             char line_type;
@@ -81,22 +84,10 @@ int main(int argc, const char * argv[]) {
           }
 
         }
-  } else {
-        std::cout << "input stream is niet open ofzo" << std::endl;
-    }
-
-  // Print vertices to see if it worked
-  int i = 0;
-  for (auto const &vertex: vertices) {
-    std::cout << "Vertex " << i++ << ": " << "(" << vertex.x << ", " << vertex.y << ", " << vertex.z << ")" << std::endl;
   }
 
-  // Print faces to see if it worked
-  i = 0;
-  for (auto const &face: faces) {
-    std::cout << "Face " << i++ << ": ";
-    for (auto const &vertex: face.boundary) std::cout << " " << vertex;
-    std::cout << std::endl;
+  for (auto vertex : vertices) {
+      std::cout << vertex.x << " " << vertex.y << " " << vertex.z << std::endl;
   }
 
   // Step 2: For each of the faces compute the best-fitting plane
@@ -112,6 +103,61 @@ int main(int argc, const char * argv[]) {
   }
 
   // Step 3: Triangulate faces
+  for (auto &face : faces) {
+      // For every face fit points onto 2d
+      std::vector<Point2> facePoints2D;
+
+      for (int vertex : face.boundary) {
+          Point3 point3d(vertices[vertex].x, vertices[vertex].y, vertices[vertex].z);
+          Point2 pt = face.best_plane.to_2d(point3d);
+          facePoints2D.push_back(pt);
+      }
+
+      // create a triangulation for the facePoints2D.
+      Triangulation triangulation;
+
+      // insert all the points into the triangulation
+      for (const Point2& pt : facePoints2D) {
+          triangulation.insert(pt);
+      }
+
+      // add begin point again to facePoints2D, so we can make a closed loop with a for loop
+      facePoints2D.push_back(facePoints2D[0]);
+
+      // iterate over the points and add pairs of them as constraints
+      for (int i = 0; i < facePoints2D.size() - 1; ++i) {
+          triangulation.insert_constraint(facePoints2D[i], facePoints2D[i + 1]);
+      }
+
+      // when the triangulation is finished assign it to the face
+      face.triangulation = triangulation;
+
+      // Step 4: Label triangulation (per face)
+      std::list<Triangulation::Face_handle> to_check;
+      triangulation.infinite_face()->info().processed = true;
+      CGAL_assertion(triangulation.infinite_face()->info().processed == true); // check that the infinite face is always marked as processed
+      CGAL_assertion(triangulation.infinite_face()->info().interior == false); // check that the infinite face is always marked as exterior
+      to_check.push_back(triangulation.infinite_face());
+      while (!to_check.empty()) {
+          CGAL_assertion(to_check.front()->info().processed == true);
+          for (int neighbour = 0; neighbour < 3; ++neighbour) {
+              if (to_check.front()->neighbor(neighbour)->info().processed) {
+
+              } else {
+                  to_check.front()->neighbor(neighbour)->info().processed = true;
+                  CGAL_assertion(to_check.front()->neighbor(neighbour)->info().processed == true);
+                  if (triangulation.is_constrained(Triangulation::Edge(to_check.front(), neighbour))) {
+                      to_check.front()->neighbor(neighbour)->info().interior = !to_check.front()->info().interior;
+                      to_check.push_back(to_check.front()->neighbor(neighbour));
+                  } else {
+                      to_check.front()->neighbor(neighbour)->info().interior = to_check.front()->info().interior;
+                      to_check.push_back(to_check.front()->neighbor(neighbour));
+                  }
+              }
+          } to_check.pop_front();
+      }
+      
+  }
 
   // Label triangulation (to do)
 
